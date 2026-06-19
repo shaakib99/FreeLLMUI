7a97764f8a35d71ce7bcb959d7c530fdc296f06f
Wahid Shakib <wsakib87@gmail.com>
2026-06-19 18:59:48 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>FreeLLMUI</h1>
+      <p>Welcome to the FreeLLMUI React application.</p>
+    </div>
+  );
+}
+
+export default App;
